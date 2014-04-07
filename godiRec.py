b330c17255dbf60ced7dd9a1f31557d7f5e7218e
climax <magg3r@yahoo.de>
2014-04-07 18:32:44 +0200

    integrate non-blocking, bugg bei stop_stream()

--- godiRec.py
+++ godiRec.py
@@ -1,38 +1,66 @@
 import pyaudio
 import wave
 
-class GodiRec:
-	def __init__(self):
-		self.CHUNK = 1024
-		self.FORMAT = pyaudio.paInt16
-		self.CHANNELS = 2
-		self.RATE = 44100
-		self.RECORD_SECONDS = 5
-		self.WAVE_OUTPUT_FILENAME = "output.wav"
-		self.p = pyaudio.PyAudio()
+class Recorder(object):
+    #A recorder class for recording audio to a WAV file.
+ 
+    def __init__(self, channels=2, rate=44100, frames_per_buffer=1024):
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+ 
+    def open(self, fname, mode='wb'):
+        return RecordingFile(fname, mode, self.channels, self.rate,
+                            self.frames_per_buffer)
 
-	def recording(self):
-		stream = self.p.open(format=self.FORMAT,
-				channels=self.CHANNELS,
-				rate=self.RATE,
-				input=True,
-				frames_per_buffer=self.CHUNK)
+class RecordingFile:
+    def __init__(self, fname, mode, channels, rate, frames_per_buffer):
+        self.fname = fname
+        self.frames_per_buffer = frames_per_buffer
+        self.mode = mode
+        self.channels = channels
+        self.rate = rate
+        self.p = pyaudio.PyAudio()
+        self.wavefile = self._prepare_file(self.fname, self.mode)
+        self.stream = None
 
-		frames = []
+    #def __enter__(self):
+     #   return self
+ 
+    #def __exit__(self, exception, value, traceback):
+     #   self.close()
 
-		for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
-			    data = stream.read(self.CHUNK)
-			    frames.append(data)
+    def get_callback(self):
+        def callback(in_data, frame_count, time_info, status):
+            self.wavefile.writeframes(in_data)
+            return in_data, pyaudio.paContinue
+        return callback
 
-		print("* done recording")
+    def start_recording(self):
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                channels=self.channels,
+                rate=self.rate,
+                input=True,
+                stream_callback=self.get_callback())
+        self.stream.start_stream()
+        print "start"
+        return self
 
-		stream.stop_stream()
-		stream.close()
-		self.p.terminate()
+    def stop_recording(self):
+        print "1"
+        self.stream.stop_stream()
+        print "stop"
+        return self
 
-		wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
-		wf.setnchannels(self.CHANNELS)
-		wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
-		wf.setframerate(self.RATE)
-		wf.writeframes(b''.join(frames))
-		wf.close()
+    def close(self):
+        print "closing"
+        self.stream.close()
+        self.p.terminate()
+        self.wavefile.close()
+
+    def _prepare_file(self, fname, mode='wb'):
+        wavefile = wave.open(fname, mode)
+        wavefile.setnchannels(self.channels)
+        wavefile.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
+        wavefile.setframerate(self.rate)
+        return wavefile
--- gui.py
+++ gui.py
@@ -1,21 +1,9 @@
 import os
 import sys
-import threading
+import time
 from PyQt4 import QtGui, QtCore
-from godiRec import GodiRec
+from godiRec import Recorder
 
-class myThread (threading.Thread):
-	def __init__(self, threadID, name, counter, rec):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.counter = counter
-		self.rec = rec
-		print "in"
-	def run(self):
-		print "Starting " + self.name
-		self.rec.recording()
-		print "Exiting " + self.name
 
 class RecGui(QtGui.QWidget):
 	exitFlag = 0
@@ -23,7 +11,9 @@ class RecGui(QtGui.QWidget):
 	def __init__(self):
 		super(RecGui, self).__init__()
 		self.init_ui()
-		self.rec = GodiRec() 
+		rec = Recorder(channels=2) 
+		#with rec.open('nonblocking.wav', 'wb') as self.recfile2:
+		self.recfile = rec.open('nonblocking.wav', 'wb')
 
 	def init_ui(self):
 		self.setWindowTitle('GodiRec')
@@ -48,21 +38,35 @@ class RecGui(QtGui.QWidget):
 		self.grid.addWidget(self.btn_stop, 1, 3)
 		self.grid.addWidget(self.btn_save, 2, 1)
 		self.btn_rec.clicked.connect(self.buttonRec)
-		self.btn_play.clicked.connect(self.buttonRec)
-		self.btn_stop.clicked.connect(self.buttonRec)
-		self.btn_save.clicked.connect(self.buttonRec)
+		self.btn_play.clicked.connect(self.buttonPlay)
+		self.btn_stop.clicked.connect(self.buttonStop)
+		#self.btn_save.clicked.connect(self.buttonRec)
 
 
 	def buttonRec(self):
 		self.btn_rec.setText("Recording")
 		self.btn_rec.setEnabled(False)
-		self.btn_play.setEnabled(False)
+		self.btn_play.setText("Pause")
 		self.btn_save.setEnabled(False)
-		try:
-			thread1 = myThread(1, "record", 1, self.rec)
-			thread1.start()
-		except:
-			print "Unexpected error:", sys.exc_info()[0]
+		#self.recfile2.start_recording()		
+		self.recfile = self.recfile.start_recording()
+
+	def buttonStop(self):
+		self.btn_rec.setText("Rec")
+		self.btn_rec.setEnabled(True)
+		self.btn_play.setEnabled(True)
+		self.btn_save.setEnabled(True)
+		#self.recfile2.stop_recording()
+		#self.recfile.stop_recording()
+		self.recfile.close()
+
+	def buttonPlay(self):
+		self.btn_play.setText("Play")
+		self.btn_rec.setText("Rec")
+		self.btn_rec.setEnabled(False)
+		self.btn_save.setEnabled(False)
+		print "pause"
+		self.recfile.stream.stop_stream()
 
 def run_gui():
 	app = QtGui.QApplication(sys.argv)
