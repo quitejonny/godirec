ed7107cd802c3ad7808dfe4b907339b87e92360c
Johannes Roos <johannes.roos@rwth-aachen.de>
2014-06-04 16:58:18 +0200

    changed setup.py to use distutils

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import os
-import setuptools.extension
-from cx_Freeze import setup, Executable
-# from distutils.core import setup
+# import setuptools.extension
+# from cx_Freeze import setup, Executable
+from distutils.core import setup
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -12,14 +12,14 @@ if sys.platform == "win32":
 else:
     base = None
 
-executables = [
-    Executable("gui_ui.py",
-               copyDependentFiles=True,
-               appendScriptToExe=True,
-               appendScriptToLibrary=False,
-               base=base,
-               )
-]
+# executables = [
+#     Executable("gui_ui.py",
+#                copyDependentFiles=True,
+#                appendScriptToExe=True,
+#                appendScriptToLibrary=False,
+#                base=base,
+#                )
+# ]
 
 buildOptions = dict(create_shared_zip=False,
         include_files=[os.path.join('ui', n) for n in os.listdir('ui')]
@@ -34,5 +34,7 @@ setup(
     data_files=[('ui', [os.path.join('ui', n) for n in os.listdir('ui')])
     ],
     options=dict(build_exe=buildOptions),
-    executables=executables,
+    # executables=executables,
+    # script=["godirec.py"],
+    packages=["godirec"],
 )
