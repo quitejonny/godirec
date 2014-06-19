c9beccfbb3a67e427a28e41135fe391a1564f96a
Johannes Roos <johannes.roos@rwth-aachen.de>
2014-06-19 23:56:31 +0200

    made function handle_exception python3 style

--- godirec/__init__.py
+++ godirec/__init__.py
@@ -15,29 +15,28 @@ def run_gui():
     godirec.gui.run_gui()
 
 def handle_exception(exc_type, exc_value, exc_traceback):
-  """ handle all exceptions """
-
-  ## KeyboardInterrupt is a special case.
-  ## We don't raise the error dialog when it occurs.
-  if issubclass(exc_type, KeyboardInterrupt):
-    if QtGui.qApp:
-      QtGui.qApp.quit()
-    return
-
-  filename, line, dummy, dummy = traceback.extract_tb( exc_traceback ).pop()
-  filename = os.path.basename( filename )
-  error    = "%s: %s" % ( exc_type.__name__, exc_value )
-
-  QtGui.QMessageBox.critical(None,"Error",
-    "<html>A critical error has occured.<br/> "
-  + "<b>%s</b><br/><br/>" % error
-  + "It occurred at <b>line %d</b> of file <b>%s</b>.<br/>" % (line, filename)
-  + "For more information see error log file"
-  + "</html>")
-
-  logging.error("".join(traceback.format_exception(exc_type, exc_value,
-                                                   exc_traceback)))
-  sys.exit(1)
+    """ handle all exceptions """
+    # KeyboardInterrupt is a special case.
+    # We don't raise the error dialog when it occurs.
+    if issubclass(exc_type, KeyboardInterrupt):
+        if QtGui.qApp:
+            QtGui.qApp.quit()
+        return
+    filename, line, _, _ = traceback.extract_tb(exc_traceback).pop()
+    filename = os.path.basename(filename)
+    error = "{}: {}".format(exc_type.__name__, exc_value)
+    QtGui.QMessageBox.critical(
+        None, "Error",
+        ("<html>A critical error has occured.<br/> "
+        "<b>{:s}</b><br/><br/>"
+        "It occurred at <b>line {:d}</b> of file <b>{:s}</b>.<br/>"
+        "For more information see error log file"
+        "</html>").format(error, line, filename)
+    )
+    logging.error(
+        "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+    )
+    sys.exit(1)
 
 _log_conf_file = resource_filename(__name__, "data/log/log.conf")
 logging.config.fileConfig(_log_conf_file, disable_existing_loggers=False)
