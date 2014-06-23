ed87a407542f894d840a4f201e091407287f56d1
Johannes Roos <johannes.roos@rwth-aachen.de>
2014-06-23 19:42:36 +0200

    changed setup.py again to use py2exe in hope to get it finally to work under Windows

--- setup.py
+++ setup.py
@@ -2,41 +2,40 @@
 import sys
 import os
 import glob
-from pkg_resources import resource_filename
 import godirec
-import setuptools.extension
-from setuptools import find_packages
-from cx_Freeze import setup, Executable
-
+from setuptools import setup, find_packages
+# import setuptools.extension
+# from cx_Freeze import setup, Executable
+# 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
-if sys.platform == "win32":
-    base = "Win32GUI"
-else:
-    base = None
-
-executables = [
-    Executable("run_gui.py",
-               copyDependentFiles=True,
-               appendScriptToExe=True,
-               appendScriptToLibrary=False,
-               base=base,
-               compress=True,
-               )
-]
-
-options = {
-    "build_exe": {
-        "packages": [
-            #"godirec",
-        ],
-        "zip_includes": glob.glob("godirec/data/*/*"),
-        "create_shared_zip": False,
-        "include_in_shared_zip": False,
-        "optimize": 2,
-        "compressed": True,
-    }
-}
+# if sys.platform == "win32":
+#     base = "Win32GUI"
+# else:
+#     base = None
+# 
+# extra_setup["executables"] = [
+#     Executable("run_gui.py",
+#                copyDependentFiles=True,
+#                appendScriptToExe=True,
+#                appendScriptToLibrary=False,
+#                base=base,
+#                compress=True,
+#                )
+# ]
+# 
+# extra_setup["options"] = {
+#     "build_exe": {
+#         "packages": [
+#             #"godirec",
+#         ],
+#         "zip_includes": [(f, f) for f in glob.glob("godirec/data/*/*")],
+#         "create_shared_zip": True,
+#         "include_in_shared_zip": False,
+#         "optimize": 2,
+#         "compressed": True,
+#     }
+# }
 
 # buildOptions = dict(create_shared_zip=False,
 #         include_files=[os.path.join('ui', n) for n in os.listdir('ui')]
@@ -44,29 +43,34 @@ options = {
 
 
 extra_setup = dict()
-# 
-# if sys.platform == 'win32':
-#     import py2exe
-#     sys.argv.append('py2exe')
-#     extra_setup['options'] = {
-#         'py2exe': {
-#             'bundle_files': 1,
-#             'compressed': True,
-#             "includes": [
-#                 "sip",
-#                 "readline",
-#                 "win32api",
-#                 "win32con"
-#             ],
-#             'zipfile': None,
-#         },
-#     }
-#     extra_setup['windows'] = {
-#         'script': "godirec/gui.py",
-#         "icon_resources": [
-#             (1, resource_filename(godirec.__name__, "data/ui/microphone2.ico"))
-#         ],
-#     }
+
+if sys.platform == 'win32':
+    import py2exe
+    sys.argv.append('py2exe')
+    extra_setup['options'] = {
+        'py2exe': {
+            'bundle_files': 0,
+            'compressed': True,
+            "excludes": [
+                "readline",
+                "win32api",
+                "win32con"
+            ],
+            "includes": [
+                "sip",
+            ],
+        },
+    }
+    extra_setup['windows'] = {
+        'script': "godirec/gui.py",
+        "icon_resources": [
+            (1, "godirec/data/ui/microphone2.ico")
+        ],
+    }
+    extra_setup['zipfile'] = None
+    extra_setup['data_files'] = [
+        (os.path.dirname(f), f) for f in glob.glob("godirec/data/*/*")
+    ]
 
 
 setup(
@@ -77,16 +81,13 @@ setup(
     author_email = 'daniel.supplieth@gmx.de',
     packages = find_packages(),
     include_package_data = True,
-    # zipfile = None,
     entry_points = {
-        'gui_scripts': ['godirec=godirec:run_gui'],
+        'gui_scripts': ['godirec=godirec.gui:run'],
     },
     install_requires = [
         'pydub',
         'setuptools',
         'mutagen',
     ],
-    executables=executables,
-    options=options,
     **extra_setup
 )
